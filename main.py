b6aad8e2d043a8fb4076b7906e50602cdbbada0d
Jacopo Di Matteo <jacopo.michele.dimatteo@gmail.com>
2023-02-15 17:21:01 +0100

    Broken code in this commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Author:     jmdm
+Date:       2023-02-15
+OS:         macOS 12.6 (Monterey)
+Hardware:   M1 chip
+This code is provided "As Is"
+"""
+
+# Standard libraries
+
+# Third-party libraries
+
+# Local libraries
+
+
+def main() -> None:
+    print("Broken Code!")
+
+
+if __name__ == "__main__":
+    main()
